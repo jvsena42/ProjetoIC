2c3c628d65145864e9e587d75cbdd992b13eb0f4
jvsena42 <jvsena16@gmail.com>
2020-03-14 22:07:24 -0300

    +Mudança de layout +2 icones do circulo

--- app/src/main/java/com/app/projetoic/TelaQuadradoActivity.java
+++ app/src/main/java/com/app/projetoic/TelaQuadradoActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelaQuadradoActivity extends AppCompatActivity {
 
     private EditText editTextLado;
+    private TextView textViewArea;
+    private TextView textViewPerimetro;
+    private TextView textViewIx;
+    private TextView textViewIy;
+    private TextView textViewix;
+    private TextView textViewiy;
+    private TextView textViewZx;
+    private TextView textViewZy;
+    private Button buttonCalcular;
 
 
     @Override
@@ -21,6 +32,20 @@ public class TelaQuadradoActivity extends AppCompatActivity {
         toolbar.setTitle("Geometrix");
         toolbar.setTitleTextColor(getColor(R.color.titleColor));
         setSupportActionBar(toolbar);
+
+        //Configuracoes iniciais
+        editTextLado = findViewById(R.id.editTextLado);
+        textViewArea = findViewById(R.id.textViewArea);
+        textViewPerimetro = findViewById(R.id.textViewPerimetro);
+        textViewIx = findViewById(R.id.textViewIx);
+        textViewIy = findViewById(R.id.textViewIy);
+        textViewix = findViewById(R.id.textViewix);
+        textViewiy = findViewById(R.id.textViewiy);
+        textViewZx = findViewById(R.id.textViewZx);
+        textViewZy = findViewById(R.id.textViewZy);
+        buttonCalcular = findViewById(R.id.buttonCalcular);
+
+
 
     }
 }
