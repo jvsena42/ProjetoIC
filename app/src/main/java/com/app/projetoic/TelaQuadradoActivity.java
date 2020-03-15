d72138c61678ba348c45d1690b7e73666025fe73
jvsena42 <jvsena16@gmail.com>
2020-03-15 19:34:48 -0300

    +Modificações nos icones

--- app/src/main/java/com/app/projetoic/TelaQuadradoActivity.java
+++ app/src/main/java/com/app/projetoic/TelaQuadradoActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TelaQuadradoActivity extends AppCompatActivity {
 
@@ -45,9 +47,55 @@ public class TelaQuadradoActivity extends AppCompatActivity {
         textViewZy = findViewById(R.id.textViewZy);
         buttonCalcular = findViewById(R.id.buttonCalcular);
 
+        //Evento de clique
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //Recuperar dados
+                String textLado = editTextLado.getText().toString();
+                if (!textLado.isEmpty()){
+                    float medidaLado = Float.parseFloat(textLado);
 
+                    //Área
+                    float area = medidaLado*medidaLado;
+                    String textArea = String.valueOf(area);
+                    textViewArea.setText("Área = " +textArea);
 
+                    //Perímetro
+                    float perimetro = medidaLado*4;
+                    String textPerimetro = String.valueOf(perimetro);
+                    textViewPerimetro.setText("Perímetro = " + textPerimetro);
+
+                    //Momento de inercia
+                    float momentoInercia = (float) (Math.pow(medidaLado,4)/12);
+                    String textMomentoInercia = String.valueOf(momentoInercia);
+                    textViewIx.setText("Ix = " + textMomentoInercia);
+                    textViewIy.setText("Iy = " + textMomentoInercia);
+
+                    //Raio de giração
+                    float raioGiracao = (float) (medidaLado/Math.sqrt(12));
+                    String textRaioGiracao = String.valueOf(raioGiracao);
+                    textViewix.setText("ix = " + textRaioGiracao);
+                    textViewiy.setText("iy = " + textRaioGiracao);
+
+                    //Módulo Plástico
+                    float moduloPlastico = (float) (Math.pow(medidaLado,3)/4);
+                    String textModuloPlastico = String.valueOf(moduloPlastico);
+                    textViewZx.setText("Zx = "+textModuloPlastico);
+                    textViewZy.setText("Zy = "+textModuloPlastico);
+
+                    //Apagar caixa de Edittext
+                    editTextLado.setText("");
+
+                }else {
+                    Toast.makeText(TelaQuadradoActivity.this, "Preencha todos os valores!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
+
+
 }
