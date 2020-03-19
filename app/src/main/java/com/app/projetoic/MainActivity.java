12b3d512fab3a561735a0e6e3818b06483d94361
jvsena42 <jvsena16@gmail.com>
2020-03-18 21:44:17 -0300

    interface de clique

--- app/src/main/java/com/app/projetoic/MainActivity.java
+++ app/src/main/java/com/app/projetoic/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView imageQuadrado;
     private ImageView imageQuadradoVazado;
@@ -37,10 +37,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageQuadrado:
+                Intent intent = new Intent(MainActivity.this, TelaQuadradoActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.imageQuadradoVazado:
+                Intent intent2 = new Intent(MainActivity.this, TelaQuadradoVazadoActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.imageRetangulo:
+                Intent intent3 = new Intent(MainActivity.this, TelaRetanguloActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.imageRetanguloVazado:
+                break;
+
+        }
+    }
+
+    private void inicializarItens() {
+        imageQuadrado = findViewById(R.id.imageQuadrado);
+        imageQuadradoVazado = findViewById(R.id.imageQuadradoVazado);
+        imageRetangulo = findViewById(R.id.imageRetangulo);
+        imageRetanguloVazado = findViewById(R.id.imageRetanguloVazado);
+
+        imageQuadrado.setOnClickListener(this);
+        imageRetanguloVazado.setOnClickListener(this);
+        imageRetangulo.setOnClickListener(this);
+        imageQuadradoVazado.setOnClickListener(this);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main,menu);
+        inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
 
     }
@@ -48,39 +81,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.itemConfiguracoes:
-                Toast.makeText(getApplicationContext(),"Página configurações",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Página configurações", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.itemSobre:
-                Intent intent = new Intent(MainActivity.this,SobreActivity.class);
+                Intent intent = new Intent(MainActivity.this, SobreActivity.class);
                 startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Página sobre",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Página sobre", Toast.LENGTH_SHORT).show();
                 break;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void telaQuadrado (View view){
-        Intent intent = new Intent(MainActivity.this,TelaQuadradoActivity.class);
-        startActivity(intent);
-    }
-
-    public void telaQuadradoVazado (View view){
-        Intent intent = new Intent(MainActivity.this,TelaQuadradoVazadoActivity.class);
-        startActivity(intent);
-    }
-
-    public void telaRetangulo (View view){
-        Intent intent = new Intent(MainActivity.this,TelaRetanguloActivity.class);
-        startActivity(intent);
-    }
-
-    private void inicializarItens(){
-        imageQuadrado = findViewById(R.id.imageQuadrado);
-        imageQuadradoVazado = findViewById(R.id.imageQuadradoVazado);
-        imageRetangulo = findViewById(R.id.imageRetangulo);
-        imageRetanguloVazado = findViewById(R.id.imageRetanguloVazado);
-    }
 }
